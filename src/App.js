377190d318dea50517dd4dd86b9e54e80c788fe8
fabio <fabiolukascj@gmail.com>
2021-04-08 22:59:36 -0400

    1-passo Feito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <p></p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount(){
+    this.carregarPosts()
+  }
+
+  carregarPosts = async () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(posts => this.setState({ posts: posts }))
+    
+  }
+
+  render() {
+    let {posts} = this.state
+    return (
+      <div className="App">
+          {posts.map(post => (
+            <div className="Post">
+              <h3 key={post.id}>{post.id} - {post.title}</h3>
+              <p>{post.body}</p>
+            </div>
+          ))}
+      </div>
+    )
+  }
 }
-
 export default App;
